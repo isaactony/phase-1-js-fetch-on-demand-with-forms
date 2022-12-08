4601d00c634feadebb25e8e90d675929aa9ad7cf
isaac <isaactonyloi@gmail.com>
2022-12-08 11:31:04 +0300

    fetching on demand with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
   
-}
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      //const input = event.target.children[1].value;
 
-document.addEventListener('DOMContentLoaded', init);
+      const input = document.querySelector('input#searchByID');
+  
+      //console.log(input.value);
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(response => response.json())
+      .then(data => {
+        updateDataFromAPI(data);
+      });
+    });
+  }
+
+  function updateDataFromAPI(data) {
+    const h3 = document.querySelector("section#movieDetails h4");
+    const p = document.querySelector("section#movieDetails p");
+
+    h3.textContent = data.title
+    p.textContent = data.summary
+
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
